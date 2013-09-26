b692676677056ee20cea0ce50f232dc88fbd53a2
Arturo Balbín Concar S. A. <r2d2klapa@gmail.com>
2013-09-26 14:57:57 -0500

    Revision 2

--- App/TestsApp/TestsApp/Controllers/HomeController.cs
+++ App/TestsApp/TestsApp/Controllers/HomeController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestsApp.Filters;
 
 namespace TestsApp.Controllers
 {
+    [Authorize]
+    [InitializeSimpleMembership]
     public class HomeController : Controller
     {
         public ActionResult Index()
